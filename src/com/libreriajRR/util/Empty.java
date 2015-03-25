c10c020812a31850ad0d2798dd21fdc813edc548
jairo <jairo@jairo-VirtualBox>
2015-03-25 14:38:33 -0400

    clara empty

---
+++ src/com/libreriajRR/util/Empty.java
@@ -0,0 +1,9 @@
+
+package com.libreriajRR.util;
+
+
+public class Empty {
+    public static final String emptyString = "";
+    public static final boolean emptyBoolean = false;
+    public static final int emptyInteger = 0;   
+}
