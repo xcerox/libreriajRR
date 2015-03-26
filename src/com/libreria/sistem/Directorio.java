34af1be2c3c4734eabd82317a742eed59a1eac0c
jairo <jairo@jairo-VirtualBox>
2015-03-26 07:48:32 -0400

    Agregacion de un nuevo package "sistema" donde se alamcenaran las funcionalidades consernientes al sistema tambien la agregacion de una nueva clase directorio que permite buscar archivos dentro de una ruta

---
+++ src/com/libreria/sistem/Directorio.java
@@ -0,0 +1,61 @@
+package com.libreria.sistem;
+
+import com.library.util.Empty;
+import java.io.File;
+import java.util.ArrayList;
+
+public class Directorio {
+
+    {
+        ext = Empty.emptyString;
+        buscarEnSubCarpetas = Empty.emptyBoolean;
+    }
+
+    private String ext;
+    private boolean buscarEnSubCarpetas;
+    
+    private File[] getArchivosEnDirectorio(File Directorio){
+        File[] archivos = Directorio.listFiles();
+        ArrayList<File> listaArchivos = new ArrayList<>();
+        
+
+        for (File archivo:archivos) {
+            if(archivo.isDirectory() && buscarEnSubCarpetas){
+                for (File archivoEnDirectorio : getArchivosEnDirectorio(archivo)) {
+                   if (archivoEnDirectorio.getName().endsWith(ext)) 
+                        listaArchivos.add(archivoEnDirectorio);
+                }
+            }else{
+                if (archivo.getName().endsWith(ext)) 
+                    listaArchivos.add(archivo);
+            }
+        }
+        
+        return listaArchivos.toArray( new File[0]);
+        
+    }
+
+    public File[] getArchivos(String path) {
+
+        File Directorio = new File(path);
+
+        if (Directorio.exists()) 
+            return getArchivosEnDirectorio(Directorio);
+        else
+            return new File[0];
+            
+        
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
+    public void setBuscarEnSubCarpetas(boolean buscarEnSubCarpetas) {
+        this.buscarEnSubCarpetas = buscarEnSubCarpetas;
+    }
+    
+    void dispose(){
+        
+    }
+}
